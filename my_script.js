c9027156c3d295d1bbf6b3a1a742a7f66f9c0608
facundodg <facundo_dg10@hotmail.com>
2023-09-05 00:12:27 -0300

    primer commit

--- my_script.js
+++ my_script.js
@@ -1,7 +1,40 @@
 const mensaje = document.querySelector("div");
 const boton = document.getElementById("cambiar-mensaje");
 
-boton.addEventListener("click", () => {
-  mensaje.textContent = "Nuevo mensaje";
-  alert("works")
+boton.addEventListener("click", async () => {
+  try {
+
+    const data = await consulta();
+
+    mensaje.textContent = JSON.stringify(data, null, 2);
+
+  } catch (error) {
+
+    console.error("Error al obtener y mostrar el JSON:", error);
+  }
+
 });
+
+
+  async function consulta() {
+  
+    let headersList = {
+      "Accept": "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+  
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+      method: "GET",
+      headers: headersList
+    });
+  
+    let data = await response.json();
+
+    console.log(data);
+  
+    return (data)
+  
+  }
+
+
+
